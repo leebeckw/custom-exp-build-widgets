d5872cd5aeb1559584c214542248a6172480c0a7
Ashley <atung@hmc.edu>
2022-02-07 16:41:35 -0800

    added third dnd widget

---
+++ widgets/Third DnD/src/runtime/index.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import {BaseProvider, LightTheme} from 'baseui';
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
+
+import Example from "./example.js";
+
+const engine = new Styletron();
+
+function App() {
+  return <Example />;
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StyletronProvider value={engine}>
+    <BaseProvider theme={LightTheme}>
+      <App />
+    </BaseProvider>
+  </StyletronProvider>,
+  rootElement
+);
