57d509e9532b8e5dabd08f9656fa8537f2f02aaf
Ashley <atung@hmc.edu>
2022-02-09 15:41:56 -0800

    Added map functioanlity to beautiful dnd widget

--- widgets/Beautiful DnD/src/config.ts
+++ widgets/Beautiful DnD/src/config.ts
@@ -1,7 +1,7 @@
 import { ImmutableObject } from 'seamless-immutable'
 
 export interface Config {
-  exampleConfigProperty: string
+  zoomToLayer: boolean;
 }
 
 export type IMConfig = ImmutableObject<Config>
--- widgets/Beautiful DnD/src/runtime/widget.tsx
+++ widgets/Beautiful DnD/src/runtime/widget.tsx
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import FeatureLayer from "esri/layers/FeatureLayer";
+import { JimuMapViewComponent, JimuMapView, loadArcGISJSAPIModules } from "jimu-arcgis";
+import { AllWidgetProps, jsx, css } from "jimu-core";
+import { IMConfig } from "../config";
 // uuid library (universally unique identifier)
 // for unique items
 import { v4 as uuid } from 'uuid';
+
+interface IState {
+  jimuMapView: JimuMapView;
+  featureLayerOnMap: FeatureLayer;
+  featureServiceUrlInput: string;
+}
 
 // unique ids for the items
 const itemsFromBackend = [
@@ -27,61 +37,149 @@ const columnsFromBackend = {
   }
 };
 
+
 // When dragged
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
+// const onDragEnd = (result, columns, setColumns) => {
+//   if (!result.destination) return;
+//   const { source, destination } = result;
 
-  // If the source and destination of an item is different
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
+//   // If the source and destination of an item is different
+//   if (source.droppableId !== destination.droppableId) {
+//     const sourceColumn = columns[source.droppableId];
+//     const destColumn = columns[destination.droppableId];
+//     const sourceItems = [...sourceColumn.items];
+//     const destItems = [...destColumn.items];
 
-    //removes the first index of the array (so things can move up)
-    const [removed] = sourceItems.splice(source.index, 1);
+//     //removes the first index of the array (so things can move up)
+//     const [removed] = sourceItems.splice(source.index, 1);
     
-    // Insert removed element
-    destItems.splice(destination.index, 0, removed);
+//     // Insert removed element
+//     destItems.splice(destination.index, 0, removed);
 
-    // Set the columns with the new items and order
-    // ... means to expand the argument or variable
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  }
+//     // Set the columns with the new items and order
+//     // ... means to expand the argument or variable
+//     setColumns({
+//       ...columns,
+//       [source.droppableId]: {
+//         ...sourceColumn,
+//         items: sourceItems
+//       },
+//       [destination.droppableId]: {
+//         ...destColumn,
+//         items: destItems
+//       }
+//     });
+
+//     // Create and add the new Feature Layer
+//     const featureLayer = new FeatureLayer({
+//       url: evt.target.value,
+//     });
+//     JimuMapView.view.map.add(featureLayer);
+//     setFeatureLayerOnMap({
+//       featureLayerOnMap: featureLayer,
+//     });
+
+//   }
   
-  // If source and destination is the same
-  else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
-};
+//   // If source and destination is the same
+//   else {
+//     const column = columns[source.droppableId];
+//     const copiedItems = [...column.items];
+//     const [removed] = copiedItems.splice(source.index, 1);
+//     copiedItems.splice(destination.index, 0, removed);
+//     setColumns({
+//       ...columns,
+//       [source.droppableId]: {
+//         ...column,
+//         items: copiedItems
+//       }
+//     });
+//   }
 
-function App() {
+
+// };
+
+export default function Widget(props: AllWidgetProps<IMConfig>) {
   // Use the solumns from the constant declared earlier
   const [columns, setColumns] = useState(columnsFromBackend);
+  const [jimuMapView, setJimuMapView] = useState(null);
+  const [featureLayerOnMap, setFeatureLayerOnMap] = useState(null);
+
+  // const featureLayer = new FeatureLayer({
+  //   url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads_Styled/FeatureServer/0"
+  // });
+  // jimuMapView.view.map.add(featureLayer);
+
+  // When dragged
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+
+    // If the source and destination of an item is different
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+
+      //removes the first index of the array (so things can move up)
+      const [removed] = sourceItems.splice(source.index, 1);
+      
+      // Insert removed element
+      destItems.splice(destination.index, 0, removed);
+
+      // Set the columns with the new items and order
+      // ... means to expand the argument or variable
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+
+      // // Create and add the new Feature Layer
+      // jimuMapView.view.map.add(featureLayer);
+      // setFeatureLayerOnMap({
+      //   featureLayerOnMap: featureLayer,
+      // });
+    }
+    
+    // If source and destination is the same
+    else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
+
 
   return (
-    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+    <div className="widget-dnd map" style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+
+        {props.hasOwnProperty("useMapWidgetIds") &&
+        props.useMapWidgetIds &&
+          props.useMapWidgetIds.length === 1 && (
+            <JimuMapViewComponent
+              useMapWidgetId={props.useMapWidgetIds?.[0]}
+              onActiveViewChange={onDragEnd}
+            />
+          )}
+
+
+
       <DragDropContext
         onDragEnd={result => onDragEnd(result, columns, setColumns)}
       >
@@ -158,4 +256,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
