aa65e6dd6da98c19ad7d7c1d809d913e5ff446b0
Ashley <atung@hmc.edu>
2022-02-21 16:04:29 -0800

    Added lockable, added layer when right order, added randomized starting list

--- widgets/Ordered Elements Add Layers/src/runtime/widget.tsx
+++ widgets/Ordered Elements Add Layers/src/runtime/widget.tsx
@@ -3,32 +3,69 @@ import { React, AllWidgetProps, jsx } from "jimu-core";
 import { JimuMapViewComponent, JimuMapView } from "jimu-arcgis";
 import FeatureLayer from "esri/layers/FeatureLayer";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
-// uuid library (universally unique identifier)
-// for unique items
-// import { v4 as uuid } from 'uuid';
+import { random } from Math;
 
-// // unique ids for the items
-// const itemsFromBackend = [
-//   { id: uuid(), content: "Item 1" },
-//   { id: uuid(), content: "Item 2" },
-//   { id: uuid(), content: "Item 3" }
-// ];
 
-// // unique column spaces
-// const columnsFromBackend = {
-//   [uuid()]: {
-//     name: "First",
-//     items: itemsFromBackend
+// const shuffle = (array) => {
+//   let currentIndex = array.length;
+//   let temporaryValue;
+//   let randomIndex;
+
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+
+//     // And swap it with the current element.
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
 //   }
+
+//   return array;
 // };
 
-const getItems = (count) =>
-  Array.from({ length: count }, (v, k) => k).map((k) => ({
-    id: `item-${k}`,
-    content: `item ${k}`
-  }));
 
-  // a little function to help us with reordering the result
+// Checks if array is properly shuffled for greater than 50% shuffled
+// True if NOT properly shuffled
+// We compare index of the item to its id
+
+// Returns true of the array and id is too similar
+// Threshold needs to be between 0 and 1
+const checkArraySimilar = (arr, threshold = 0.25) => {
+  let count = 0;
+  
+  for (const [index, element] of arr.entries()){
+    if (index == element.id) {
+      count = count + 1;
+    }
+  };
+  return count/arr.length >= threshold;
+};
+
+
+const getItems = (instructions) => {
+  let count = instructions.length;
+
+  const arr = Array.from({ length: count }, (v, k) => k).map((k) => ({
+    // Id is the order the item SHOULD be in
+    id: `${k}`,
+    content: `${instructions[k]}`
+  }))
+  const shuffled_arr = arr;
+
+  if (arr.length > 1) {
+    while (checkArraySimilar(shuffled_arr, 0.25)) {
+      shuffled_arr.sort(() => Math.random() - 0.5);
+    };
+  };
+  console.log("HELLO!!!!!");
+  return shuffled_arr
+};
+
+
+// a little function to help us with reordering the result
 const reorder = (list, startIndex, endIndex) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
@@ -39,6 +76,8 @@ const reorder = (list, startIndex, endIndex) => {
 
 const grid = 8;
 
+
+// TODO: if draggable area is locked, change color
 const getItemStyle = (isDragging, draggableStyle) => ({
   // some basic styles to make the items look a bit nicer
   userSelect: "none",
@@ -46,7 +85,7 @@ const getItemStyle = (isDragging, draggableStyle) => ({
   margin: `0 0 ${grid}px 0`,
 
   // change background colour if dragging
-  background: isDragging ? "lightgreen" : "grey",
+  background: isDragging ? "pink" : "grey",
 
   // styles we need to apply on draggables
   ...draggableStyle
@@ -69,13 +108,16 @@ export default class Widget extends React.PureComponent<AllWidgetProps<any>, any
     super(props);
     this.state = {
       jimuMapView: null,
-      items: getItems(3)
+      items: getItems(["Grab two slices of bread",
+                       "Put peanut butter on both slices",
+                       "Put jelly in the middle of both slices",
+                       "Put slices together"]),
+      isLocked: false
     };
     this.onDragEnd = this.onDragEnd.bind(this)
   }
 
   onDragEnd(result) {
-    // const { source, destination } = result;
 
     // dropped outside the list
     if (!result.destination) {
@@ -88,17 +130,21 @@ export default class Widget extends React.PureComponent<AllWidgetProps<any>, any
       result.destination.index
     );
 
-    this.setState({ items });
+    this.setState({items});
 
-    // create a new FeatureLayer
-    const layer = new FeatureLayer({
-      url: "https://services3.arcgis.com/GzteEaZqBuJ6GIYr/arcgis/rest/services/worldcities/FeatureServer"
-    });
+    if (checkArraySimilar(items, 1.0)) {
+      // create a new FeatureLayer
+      const layer = new FeatureLayer({
+        url: "https://services3.arcgis.com/GzteEaZqBuJ6GIYr/arcgis/rest/services/Buffer_of_1750_from_LA/FeatureServer"
+      });
 
-    // Add the layer to the map (accessed through the Experience Builder JimuMapView data source)
-    this.state.jimuMapView.view.map.add(layer);
+      // Add the layer to the map (accessed through the Experience Builder JimuMapView data source)
+      this.state.jimuMapView.view.map.add(layer);
 
-    
+      // Lock the draggable area
+      this.setState({isLocked: true})
+    }  
+
   };
   
 
@@ -127,7 +173,11 @@ export default class Widget extends React.PureComponent<AllWidgetProps<any>, any
                   style={getListStyle(snapshot.isDraggingOver)}
                 >
                   {this.state.items.map((item, index) => (
-                    <Draggable key={item.id} draggableId={item.id} index={index}>
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}
+                      isDragDisabled={this.state.isLocked}>
                       {(provided, snapshot) => (
                         <div
                           ref={provided.innerRef}
