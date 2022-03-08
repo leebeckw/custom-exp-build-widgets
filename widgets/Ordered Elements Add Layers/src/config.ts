3df7e6fd1dba7acfec50e735afad73b91ddfc58c
Ashley <atung@hmc.edu>
2022-03-08 09:48:57 -0800

    Added HTML parser and textbox

--- widgets/Ordered Elements Add Layers/src/config.ts
+++ widgets/Ordered Elements Add Layers/src/config.ts
@@ -4,6 +4,7 @@ export interface Config {
     instructText: Array<string>;
     layerUrls: Array<string>;
     isClicked: Boolean;
+    codeText: string;
 }
 
 export type IMConfig = ImmutableObject<Config>;
--- widgets/Ordered Elements Add Layers/src/setting/setting.tsx
+++ widgets/Ordered Elements Add Layers/src/setting/setting.tsx
@@ -1,4 +1,5 @@
 import { React } from "jimu-core";
+import { Component } from 'react';
 import { AllWidgetSettingProps } from "jimu-for-builder";
 import {
   MapWidgetSelector,
@@ -31,6 +32,7 @@ export default class Setting extends React.PureComponent<
         typeof this.props.config?.instructText === undefined,
         typeof this.props.config?.layerUrls === undefined,
         typeof this.props.config?.isClicked === undefined,
+        typeof this.props.config?.codeText === undefined
       );
 
       this.state = {
@@ -45,6 +47,10 @@ export default class Setting extends React.PureComponent<
           this.props.config?.layerUrls === undefined
             ? ""
             : this.props.config?.layerUrls.join("\n"),
+        codeTextareaValue:
+          this.props.config?.codeText === undefined
+            ? ""
+            : this.props.config?.codeText,
       };
     }
 
@@ -113,6 +119,23 @@ export default class Setting extends React.PureComponent<
     });
   };
 
+  onCodeTextChange = (event) => {
+    /**
+     * In the code text area, author should input HTML code
+     * for code pop-up
+     */
+
+    this.setState({ codeTextareaValue: event.target.value });
+
+    this.props.onSettingChange({
+      id: this.props.id,
+      config: this.props.config.set(
+        "codeText",
+        event.target.value
+      ),
+    });
+  };
+
 
   render() {
     return (
@@ -142,7 +165,7 @@ export default class Setting extends React.PureComponent<
           })}
         >
 
-          {/* Render text input area for isntructions */}
+          {/* Render text input area for instructions */}
           <SettingRow>
             <textarea
               className="w-100 p-1"
@@ -168,7 +191,6 @@ export default class Setting extends React.PureComponent<
             defaultMessage: defaultI18nMessages.toAdd,
           })}
         >
-
           {/* Render text input area to add URL to map layer */}
           <SettingRow>
             <textarea
@@ -176,6 +198,23 @@ export default class Setting extends React.PureComponent<
               style={{ whiteSpace: "nowrap", minHeight: "100px" }}
               value={this.state.layerTextareaValue}
               onChange={this.onLayerTextChange}
+            ></textarea>
+          </SettingRow>
+        </SettingSection>
+
+        <SettingSection
+          title={this.props.intl.formatMessage({
+            id: "codeArea",
+            defaultMessage: defaultI18nMessages.codeArea,
+          })}
+        >
+          {/* Render text input area to add URL to map layer */}
+          <SettingRow>
+            <textarea
+              className="w-100 p-1"
+              style={{ whiteSpace: "pre", minHeight: "100px" }}
+              value={this.state.codeText}
+              onChange={this.onCodeTextChange}
             ></textarea>
           </SettingRow>
         </SettingSection>
